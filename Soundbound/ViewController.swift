1787d7d0d241b2afbba68f5d7af58432cc4032d2
Kaarstthenn Ancco Escobar <kaarstthen.ancco@tecsup.edu.pe>
2021-10-20 10:41:25 -0500

    first commit

---
+++ Soundbound/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Soundbound
+//
+//  Created by Mac 05 on 10/20/21.
+//  Copyright © 2021 Kaarstthen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
